c37631793964dc1ba43625217938079accab3626
Varshit Dusad <dusadv@BBRs-MacBook-Pro.local>
2024-01-06 19:38:13 +0530

    Rewritten code to include caching

--- pages/experiment.py
+++ pages/experiment.py
@@ -19,28 +19,37 @@ pinecone_api = env_vars.get("PINECONE_API_KEY")
 pinecone_env = env_vars.get("PINECONE_ENV")
 
 ## 
-model = SentenceTransformer('pinecone/mpnet-retriever-squad2')
+@st.cache
+def init_retriever():
+    return SentenceTransformer('pinecone/mpnet-retriever-squad2')
 
 # Initialize connection to pinecone
-pinecone.init(
-    api_key=pinecone_api,
-    environment=pinecone_env
-)
-
-# # check if index already exists and if not we create it
-if 'qa-index' not in pinecone.list_indexes():
-    pinecone.create_index(
-        name='qa-index',
-        dimension=model.get_sentence_embedding_dimension()
+@st.cache
+def init_pinecone():
+    pinecone.init(
+        api_key=pinecone_api,
+        environment=pinecone_env
     )
 
-# # connect to the index
-index = pinecone.Index('qa-index')
+    # # check if index already exists and if not we create it
+    if 'qa-index' not in pinecone.list_indexes():
+        pinecone.create_index(
+        name='qa-index',
+        dimension=model.get_sentence_embedding_dimension()
+        )
+
+    # # connect to the index
+    index = pinecone.Index('qa-index')
+    return index
+
 
 st.write("""
 # AI Q&A
 Ask me a question!
 """)
+
+model = init_retriever()
+index = init_pinecone()
 
 query = st.text_input('Search!', '')
 
